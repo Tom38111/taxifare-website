ce75cb5d6b50397829970f0daf990524de29d7d6
Thomas PELLETIER <thomaspelletier@hotmail.com>
2024-12-06 15:10:38 +0100

    OK

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import pandas as pd
 
 '''
-# TaxiFareModel front
+# TaxiFareModel
 '''
 
 st.markdown('''
@@ -13,17 +13,9 @@ Remember that there are several ways to output content into your web page...
 Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
 ''')
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
+## Please complete the field hereafter:
+
 date = st.date_input('Enter date')
 time = st.time_input('Enter time')
 pickup_longitude = st.number_input('Enter pickup longitude')
@@ -32,46 +24,30 @@ dropoff_longitude = st.number_input('Enter dropoff longitude')
 dropoff_latitude = st.number_input('Enter dropoff latitude')
 passenger_count = st.number_input('Enter the number of passenger')
 
-'''
+
 ## Once we have these, let's call our API in order to retrieve a prediction
 
 See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
 
 🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
 
 url = 'https://taxifare.lewagon.ai/predict'
 
-if url == 'https://taxifare.lewagon.ai/predict':
+if st.button('predict fare'):
+    if all([date, time,pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude,passenger_count ]):
+        params = {'date':date, 'time':time, 'pickup_longitude' :float(pickup_longitude),'pickup_latitude' :float(pickup_latitude),
+                  'dropoff_longitude' :float(dropoff_longitude),'dropoff_latitude' :float(dropoff_latitude),
+                  'passenger_count' : int(passenger_count)}
 
-    st.markdown('Complete the missing information')
+    try:
+        reponse = requests.get(url, params=params)
 
-'''
-2. Let's build a dictionary containing the parameters for our API...'''
+        reponse.raise_for_status()
 
-dict = dict(
-        pickup_datetime=[pd.Timestamp(f'{date} {time}', tz='UTC')],
-        pickup_longitude=[pickup_longitude],
-        pickup_latitude=[pickup_latitude],
-        dropoff_longitude=[dropoff_longitude],
-        dropoff_latitude=[dropoff_latitude],
-        passenger_count=[passenger_count],
-    )
-'''
-3. Let's call our API using the `requests` package...'''
-st.markdown(dict)
+        prediction = response.json().get("fare", "No fare returned")
+            st.success(f"The predicted fare is: ${prediction:.2f}")
 
-url = f'https://taxifare.lewagon.ai/predict?{dict}'
-
-reponse = requests.get(url)
-
-st.markdown(reponse)
-
-'''
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-
-
-
-'''
+    except Exception as e:
+        st.error(f"Error calling the API: {e}")
+    else:
+        st.error("Please fill in all fields before predicting!")
